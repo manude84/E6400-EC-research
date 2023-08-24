1450b6e2f712083f09819ba65da67a8143d02523
Nicholas Chin <nic.c3.14@gmail.com>
2023-08-24 12:39:02 -0600

    enable_touchpad.c: Add command line options
    This requires that the argument for the EC command
    is passed in through the command line.

--- boot_bios_settings/post_behavior/mouse_touchpad/enable_touchpad.c
+++ boot_bios_settings/post_behavior/mouse_touchpad/enable_touchpad.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/io.h>
 
 static const unsigned short EC_CTRL = 0x910;
@@ -19,14 +20,27 @@ static void send_command(unsigned char cmd) {
 	wait_ec();
 }
 
-int main()
+int main(int argc, char *argv[])
 {
 	if (ioperm(0x910, 2, 1) < 0) {
 		perror("ioperm");
 		return -1;
 	}
+	if (argc < 2) {
+		printf("missing argument.\n");
+		printf("Usage: enable_touchpad 0|1|2\n");
+		printf("0: Disable touchpad, use a serial mouse\n");
+		printf("1: Disable touchpad when PS/2 mouse connected\n");
+		printf("2: Touchpad always enabled\n");
+		return -1;
+	}
+	int option = strtol(argv[1], NULL, 10);
+	if (option < 0 || option > 2) {
+		printf("Argument out of range\n");
+		return -1;
+	}
 	outb(0x12, EC_CTRL);
-	outb(0x02, EC_DATA);
+	outb(option, EC_DATA);
 	send_command(0x1A);
 	return 0;
 }
